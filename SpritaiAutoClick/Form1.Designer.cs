0615689f1401dfec6bdb0668d7c1d101e28364f6
Spritai <70975315+Spritai@users.noreply.github.com>
2025-07-15 02:38:39 +0200

    1.2
    Update

--- SpritaiAutoClick/Form1.Designer.cs
+++ SpritaiAutoClick/Form1.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            kryptonCustomPaletteBase1 = new Krypton.Toolkit.KryptonCustomPaletteBase(components);
+            StartButton = new Krypton.Toolkit.KryptonButton();
+            StopButton = new Krypton.Toolkit.KryptonButton();
+            kryptonCheckBox1 = new Krypton.Toolkit.KryptonCheckBox();
+            kryptonTextBox1 = new Krypton.Toolkit.KryptonTextBox();
+            kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            MsValue = new Krypton.Toolkit.KryptonNumericUpDown();
+            SuspendLayout();
+            // 
+            // kryptonCustomPaletteBase1
+            // 
+            kryptonCustomPaletteBase1.UseThemeFormChromeBorderWidth = Krypton.Toolkit.InheritBool.True;
+            // 
+            // StartButton
+            // 
+            StartButton.Location = new Point(12, 12);
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(144, 40);
+            StartButton.StateNormal.Border.Rounding = 15F;
+            StartButton.StateNormal.Border.Width = 2;
+            StartButton.StatePressed.Back.Color1 = SystemColors.ScrollBar;
+            StartButton.StatePressed.Back.Color2 = SystemColors.WindowFrame;
+            StartButton.StatePressed.Border.Rounding = 18F;
+            StartButton.StatePressed.Border.Width = 2;
+            StartButton.StateTracking.Back.Color1 = SystemColors.ActiveCaption;
+            StartButton.StateTracking.Back.Color2 = SystemColors.ActiveBorder;
+            StartButton.StateTracking.Border.Color1 = SystemColors.ControlDark;
+            StartButton.StateTracking.Border.Color2 = SystemColors.ControlDarkDark;
+            StartButton.StateTracking.Border.Rounding = 15F;
+            StartButton.StateTracking.Border.Width = 2;
+            StartButton.TabIndex = 0;
+            StartButton.Values.DropDownArrowColor = Color.Empty;
+            StartButton.Values.Text = "START";
+            StartButton.Click += StartButton_Click;
+            // 
+            // StopButton
+            // 
+            StopButton.Location = new Point(172, 12);
+            StopButton.Name = "StopButton";
+            StopButton.Size = new Size(144, 40);
+            StopButton.StateNormal.Border.Rounding = 15F;
+            StopButton.StateNormal.Border.Width = 2;
+            StopButton.StatePressed.Back.Color1 = SystemColors.ScrollBar;
+            StopButton.StatePressed.Back.Color2 = SystemColors.WindowFrame;
+            StopButton.StatePressed.Border.Rounding = 18F;
+            StopButton.StatePressed.Border.Width = 2;
+            StopButton.StateTracking.Back.Color1 = SystemColors.ActiveCaption;
+            StopButton.StateTracking.Back.Color2 = SystemColors.ActiveBorder;
+            StopButton.StateTracking.Border.Color1 = SystemColors.ControlDark;
+            StopButton.StateTracking.Border.Color2 = SystemColors.ControlDarkDark;
+            StopButton.StateTracking.Border.Rounding = 15F;
+            StopButton.StateTracking.Border.Width = 2;
+            StopButton.TabIndex = 1;
+            StopButton.Values.DropDownArrowColor = Color.Empty;
+            StopButton.Values.Text = "STOP";
+            StopButton.Click += StopButton_Click;
+            // 
+            // kryptonCheckBox1
+            // 
+            kryptonCheckBox1.Location = new Point(138, 58);
+            kryptonCheckBox1.Name = "kryptonCheckBox1";
+            kryptonCheckBox1.RightToLeft = RightToLeft.No;
+            kryptonCheckBox1.Size = new Size(57, 20);
+            kryptonCheckBox1.TabIndex = 2;
+            kryptonCheckBox1.Values.Text = "Status";
+            kryptonCheckBox1.CheckedChanged += kryptonCheckBox1_CheckedChanged;
+            // 
+            // kryptonTextBox1
+            // 
+            kryptonTextBox1.Location = new Point(87, 84);
+            kryptonTextBox1.Name = "kryptonTextBox1";
+            kryptonTextBox1.ReadOnly = true;
+            kryptonTextBox1.Size = new Size(166, 35);
+            kryptonTextBox1.StateCommon.Border.Rounding = 15F;
+            kryptonTextBox1.StateCommon.Border.Width = 2;
+            kryptonTextBox1.TabIndex = 3;
+            kryptonTextBox1.Text = "Enter Key";
+            kryptonTextBox1.TextAlign = HorizontalAlignment.Center;
+            kryptonTextBox1.TextChanged += kryptonTextBox1_TextChanged;
+            // 
+            // kryptonLabel1
+            // 
+            kryptonLabel1.Location = new Point(9, 357);
+            kryptonLabel1.Margin = new Padding(0);
+            kryptonLabel1.Name = "kryptonLabel1";
+            kryptonLabel1.Size = new Size(128, 20);
+            kryptonLabel1.TabIndex = 4;
+            kryptonLabel1.Values.Text = "Spritai's AutoClick 1.2";
+            kryptonLabel1.Click += kryptonLabel1_Click;
+            // 
+            // MsValue
+            // 
+            MsValue.Increment = new decimal(new int[] { 1, 0, 0, 0 });
+            MsValue.Location = new Point(104, 125);
+            MsValue.Maximum = new decimal(new int[] { 100, 0, 0, 0 });
+            MsValue.Minimum = new decimal(new int[] { 5, 0, 0, 0 });
+            MsValue.Name = "MsValue";
+            MsValue.Size = new Size(120, 22);
+            MsValue.TabIndex = 5;
+            MsValue.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            MsValue.ValueChanged += MsValue_ValueChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(328, 386);
+            Controls.Add(MsValue);
+            Controls.Add(kryptonLabel1);
+            Controls.Add(kryptonTextBox1);
+            Controls.Add(kryptonCheckBox1);
+            Controls.Add(StopButton);
+            Controls.Add(StartButton);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Spritai's AutoClick";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Krypton.Toolkit.KryptonCustomPaletteBase kryptonCustomPaletteBase1;
+        private Krypton.Toolkit.KryptonButton StartButton;
+        private Krypton.Toolkit.KryptonButton StopButton;
+        private Krypton.Toolkit.KryptonCheckBox kryptonCheckBox1;
+        private Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonNumericUpDown MsValue;
     }
 }
--- SpritaiAutoClick/Form1.cs
+++ SpritaiAutoClick/Form1.cs
@@ -1,10 +1,126 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
 namespace SpritaiAutoClick
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer clickTimer;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, Keys vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int WM_HOTKEY = 0x0312;
+        private const uint MOD_NONE = 0x0000;
+        private int currentHotkeyId = 9000;
+        private Keys currentHotkeyKey = Keys.F6;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
         public Form1()
         {
             InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            clickTimer = new System.Windows.Forms.Timer();
+            clickTimer.Interval = (int)MsValue.Value;
+            clickTimer.Tick += ClickTimer_Tick;
+
+            kryptonTextBox1.ReadOnly = false;
+            kryptonTextBox1.Text = currentHotkeyKey.ToString();
+            kryptonTextBox1.KeyDown += kryptonTextBox1_KeyDown;
+
+            kryptonCheckBox1.AutoCheck = false;
+            RegisterHotKey(this.Handle, currentHotkeyId, MOD_NONE, currentHotkeyKey);
+        }
+
+        private void kryptonTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            UnregisterHotKey(this.Handle, currentHotkeyId);
+
+
+            currentHotkeyKey = e.KeyCode;
+            kryptonTextBox1.Text = currentHotkeyKey.ToString();
+
+            RegisterHotKey(this.Handle, currentHotkeyId, MOD_NONE, currentHotkeyKey);
+
+            e.SuppressKeyPress = true;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == currentHotkeyId)
+            {
+                kryptonCheckBox1.Checked = !kryptonCheckBox1.Checked;
+            }
+            base.WndProc(ref m);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, currentHotkeyId);
+            base.OnFormClosing(e);
+        }
+
+        private void ClickTimer_Tick(object sender, EventArgs e)
+        {
+            DoMouseClick();
+        }
+
+        private void DoMouseClick()
+        {
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            kryptonCheckBox1.Checked = true;
+            clickTimer.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            kryptonCheckBox1.Checked = false;
+            clickTimer.Stop();
+        }
+
+        private void kryptonCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (kryptonCheckBox1.Checked)
+                clickTimer.Start();
+            else
+                clickTimer.Stop();
+        }
+
+        private void kryptonTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delayNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MsValue_ValueChanged(object sender, EventArgs e)
+        {
+            clickTimer.Interval = (int)MsValue.Value;
         }
     }
 }
